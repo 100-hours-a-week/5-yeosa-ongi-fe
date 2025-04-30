5586b165e40be2deb42d03635310e3982a824879
zwen-yirochi <jijohn.vega@gmail.com>
2025-04-30 19:19:51 +0900

    feat/#10 로그인 redirect 구현

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,20 @@
-import { Link } from "react-router-dom";
 export default function Home() {
+	const KAKAO_CLIENT_ID = import.meta.env.VITE_KAKAO_REST_API_KEY;
+	const REDIRECT_URI = "http://localhost:5173/api/auth/kakao/callback";
+	const kakaoLoginUrl = `https://kauth.kakao.com/oauth/authorize?client_id=${KAKAO_CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code`;
+	const handleLogin = () => {
+		window.location.href = kakaoLoginUrl;
+	};
+	console.log("a", KAKAO_CLIENT_ID);
 	return (
-		<>
-			<Link to="/login">로그인 페이지로 이동</Link>
-		</>
+		<div className="container flex flex-col items-center justify-center min-h-screen px-4 mx-auto">
+			<img className="w-[120px]" src="src/assets/ongi_Logo.png"></img>
+			<div className="mt-[100px]">카카오톡으로 간편 로그인</div>
+			<button onClick={handleLogin}>
+				<img
+					className="hover:cursor-pointer "
+					src="src/assets/kakao_login_medium_narrow.png"></img>
+			</button>
+		</div>
 	);
 }
