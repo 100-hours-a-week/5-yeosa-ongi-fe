838f5e17d9a243d5ca32ec71f2d9956239e185b2
zwen-yirochi <jijohn.vega@gmail.com>
2025-05-01 17:52:50 +0900

    feat:로그인 요청 API

---
+++ src/api/auth/login.js
@@ -0,0 +1,18 @@
+import { API_BASE_URL } from "../config";
+
+export const kakaoLogin = async () => {
+    try {
+        const apiUrl = API_BASE_URL + "/api/auth";
+        const requestOptions = {
+            method: 'GET',
+        };
+        const response = await fetch(apiUrl, requestOptions);
+
+        if (!response.ok) {
+            const errorData = await response.json().catch(() => ({}));
+            throw new Error(errorData.message || `HTTP 에러! 상태: ${response.status}`);
+        }
+    } catch (error) {
+        console.error('로그인 실패:', error.message);
+    }
+}
