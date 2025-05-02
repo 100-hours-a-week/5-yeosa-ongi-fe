efd84fa593d9e12cc2c0607f2bf3b5a5834aea6a
zwen-yirochi <jijohn.vega@gmail.com>
2025-05-02 11:09:10 +0900

    feat:chunkArrayByCol함수 추가

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -1,5 +1,24 @@
-const Grid = () => {
-	return <>Grid Comp</>;
+const Grid = ({ col = 4, items = [] }) => {
+	mockItems = [1, 2, 3, 4, 5, 6, 7];
+
+	const chunkArrayByCol = (array, chunkSize) => {
+		return Array(Math.ceil(array.length / chunkSize))
+			.fill()
+			.map((_, index) => {
+				// For each placeholder, return a slice of the original array
+				const start = index * chunkSize;
+				return array.slice(start, start + chunkSize);
+			});
+	};
+
+	const items = chunkArrayByCol(mockItems, col);
+	return (
+		<>
+			{items.map((item, index) => (
+				<div className="w-[100]px h-[100]px">{index}</div>
+			))}
+		</>
+	);
 };
 
 export default Grid;
