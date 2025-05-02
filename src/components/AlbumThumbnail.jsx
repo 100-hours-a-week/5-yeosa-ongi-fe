742a4f61ea48438735056f377f29b269b6f978d1
zwen-yirochi <jijohn.vega@gmail.com>
2025-05-02 13:06:52 +0900

    feat:디폴트 이미지 변경

--- src/components/AlbumThumbnail.jsx
+++ src/components/AlbumThumbnail.jsx
@@ -2,10 +2,10 @@ const AlbumThumbnail = ({ width = 100, height = 100 }) => {
 	const hasCoworkers = false;
 
 	return (
-		<div className="w-full h-full border-[5px] border-solid">
+		<div className="w-full h-full border border-solid">
 			<img
 				className="object-cover w-full h-full"
-				src="public/default_image_no_image.jpg"
+				src="public/default-featured-image.jpg"
 				alt="Album thumbnail"
 			/>
 			{hasCoworkers ? `<Coworker>` : ""}
