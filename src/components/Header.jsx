c83d439791d781b08e1be8033ceb63bb675c4cb0
zwen-yirochi <jijohn.vega@gmail.com>
2025-05-02 16:39:45 +0900

    feat:헤더 컴포넌트 아이콘 추가

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,16 @@
 const Header = () => {
 	return (
-		<div className="h-[80px]">
-			<img src="src/assets/ongi_logo_flat.png"></img>Header
+		<div className="h-[80px] relative">
+			<img
+				className="absolute h-[52px] left-2 top-1"
+				src="src/assets/ongi_logo_flat.png"></img>
+			<div></div>
+			<img
+				className="absolute h-[32px] right-16 top-4"
+				src="src/assets/icons/bell_icon.png"></img>
+			<img
+				src="src/assets/default_user_imgae.png"
+				className="h-[40px] right-3 absolute top-3"></img>
 		</div>
 	);
 };
