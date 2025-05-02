746b5420f06869b59023a615a6dd85000061a687
zwen-yirochi <jijohn.vega@gmail.com>
2025-05-02 13:42:12 +0900

    feat:Month 컴포넌트 생성

---
+++ src/components/Month.jsx
@@ -0,0 +1,13 @@
+import AlbumThumbnail from "./AlbumThumbnail";
+import Grid from "./Grid";
+
+const Month = () => {
+	return (
+		<>
+			<div> Title : </div>
+			<Grid ElementType={AlbumThumbnail}></Grid>
+		</>
+	);
+};
+
+export default Month;
