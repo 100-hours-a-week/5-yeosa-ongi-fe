6996e59584c8ac8ac8461c805af9f43ec3b7047a
zwen-yirochi <jijohn.vega@gmail.com>
2025-05-02 13:42:12 +0900

    feat:Month 컴포넌트 생성

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,13 +1,30 @@
-import Grid from "../components/Grid";
+import getMonthlyAlbum from "../../mock/getMonthlyAlbum.json";
+import Month from "../components/Month";
 
 const Main = () => {
+	const result = {};
+	const mockResponse = getMonthlyAlbum;
+	const albums = mockResponse.data.albums;
+	albums.forEach((album) => {
+		const date = new Date(album.createdAt);
+		const monthKey = `${date.getFullYear()}-${String(
+			date.getMonth() + 1
+		).padStart(2, "0")}`;
+
+		if (!result[monthKey]) {
+			result[monthKey] = [];
+		}
+		result[monthKey].push(album);
+	});
+	console.log(result);
+
 	return (
 		<>
 			<div className="h-[80px]">Header</div>
 			<div className="flex-col content">
 				<div className="flex w-full border-2 border-solid">지도</div>
 				<div>앨범 리스트</div>
-				<Grid />
+				<Month />
 			</div>
 		</>
 	);
