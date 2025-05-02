af16dd3ffc9cbfbe132288255a7e19e3de53d588
zwen-yirochi <jijohn.vega@gmail.com>
2025-05-02 10:31:48 +0900

    feat:앨범썸네일 레이아웃 작성

--- src/components/AlbumThumbnail.jsx
+++ src/components/AlbumThumbnail.jsx
@@ -1,8 +1,13 @@
-const AlbumThumbnail = () => {
+const AlbumThumbnail = ({ width = 100, hieght = 100 }) => {
 	const hasCoworkes = false;
 	return (
-		<div className="w-[100px]">
-			<img src="public/default_image_no_image.jpg"></img>
+		<div
+			className={`w-[${width}px] h-[${hieght}px] border-[5px] border-solid`}>
+			<img
+				className="object-cover w-full h-full"
+				src="public/default_image_no_image.jpg"
+				alt="Album thumbnail"
+			/>
 			{hasCoworkes ? `<Coworker>` : ""}
 		</div>
 	);
