b50f86c714ec5718354bae3c2bbb99698964329a
zwen-yirochi <jijohn.vega@gmail.com>
2025-05-03 20:40:05 +0900

    feat:store 파일 생성

---
+++ src/stores/selectionStore.jsx
@@ -0,0 +1,13 @@
+import { create } from "zustand";
+
+const useSelectionStore = create((set) => ({
+	selectedId: null, // 현재 선택된 항목의 ID
+
+	// 항목 선택 함수
+	selectItem: (id) => set({ selectedId: id }),
+
+	// 선택 취소 함수
+	clearSelection: () => set({ selectedId: null }),
+}));
+
+export default useSelectionStore;
