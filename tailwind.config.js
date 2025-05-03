123ecd6dde914657dfc483a0e8c9167c682ed286
zwen-yirochi <jijohn.vega@gmail.com>
2025-05-03 20:01:41 +0900

    feat:색상 템플릿 추가 및 config 변경

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,17 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: 'var(--color-primary)',
+        secondary: 'var(--color-secondary)',
+        'white-bage': 'var(--white-bage)',
+        'white-blue': 'var(--white-blue)',
+        'gray-light': 'var(--gray-light)',
+        'gray-dark': 'var(--gray-dark)',
+        'black-light': 'var(--black-light)',
+      }
+    },
   },
   plugins: [],
 }
-
