9a341b35e1eada0110f31f44816daa5e28a95bb9
zwen-yirochi <jijohn.vega@gmail.com>
2025-05-03 20:40:05 +0900

    feat:store 파일 생성

--- src/components/Month.jsx
+++ src/components/Month.jsx
@@ -1,11 +1,11 @@
 import AlbumThumbnail from "./AlbumThumbnail";
 import Grid from "./Grid";
 
-const Month = (albums) => {
-	console.log("in Month", albums);
+const Month = ({ albums, title }) => {
+	console.log("in Month", albums, title);
 	return (
 		<>
-			<div> Title : </div>
+			<div className="h-10 p-1">{title}</div>
 			<Grid ElementType={AlbumThumbnail} items={albums}></Grid>
 		</>
 	);
