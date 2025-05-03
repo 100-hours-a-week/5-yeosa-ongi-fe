d6a8be4711953657b5b2f9f9b0ca1c8e3b0273c7
zwen-yirochi <jijohn.vega@gmail.com>
2025-05-03 20:39:16 +0900

    feat:앨범 선택 기능 추가

--- src/components/AlbumThumbnail.jsx
+++ src/components/AlbumThumbnail.jsx
@@ -1,13 +1,27 @@
-const AlbumThumbnail = ({ width = 100, height = 100 }) => {
-	const hasCoworkers = false;
+import { useState } from "react";
+import useSelectionStore from "../stores/selectionStore";
 
+const AlbumThumbnail = () => {
+	const hasCoworkers = false;
+	const [selected, setSelected] = useState(false);
+	const handleSelect = () => {
+		console.log("click");
+		setSelected(true);
+		selectItem(id);
+	};
+	const { selectedId, selectItem } = useSelectionStore();
 	return (
-		<div className="w-full h-full border border-solid">
+		<div
+			className="relative w-full h-full border border-solid"
+			onClick={handleSelect}>
 			<img
 				className="object-cover w-full h-full"
 				src="public/default-featured-image.jpg"
 				alt="Album thumbnail"
 			/>
+			{selected && (
+				<div className="absolute inset-0 z-10 bg-black opacity-20"></div>
+			)}
 			{hasCoworkers ? `<Coworker>` : ""}
 		</div>
 	);
