1a5abf4f6e080eb8c5e88d960079d1fe0389eae1
zwen-yirochi <jijohn.vega@gmail.com>
2025-05-09 02:19:38 +0900

    feat:앨범상세 페이지 데이터fetch 구현

--- src/pages/Album.jsx
+++ src/pages/Album.jsx
@@ -1,9 +1,22 @@
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { getAlbumDetail } from "../api/albums/albumDetail";
 import Card from "../components/Card";
 import Category from "../components/Category";
 import Header from "../components/Header";
 
 const Album = () => {
 	const mock = [{ tag: "카테고리 1" }];
+	const { albumId } = useParams();
+
+	useEffect(() => {
+		const fetchData = async () => {
+			const response = await getAlbumDetail(albumId);
+			console.log(response);
+		};
+		fetchData();
+	}, [albumId]);
+
 	return (
 		<>
 			<Header />
