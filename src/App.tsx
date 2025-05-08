a32ad87d76163e0945020c5026e7e89c3099c360
zwen-yirochi <jijohn.vega@gmail.com>
2025-05-09 02:17:21 +0900

    feat:새로고침 시 액세스토큰 갱신 로직 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 // import KakaoCallback from './api/auth/kakao/callback';
-import axios from 'axios';
 import { useEffect } from 'react';
 import './App.css';
 import Album from './pages/Album';
@@ -11,60 +10,20 @@ import Main from './pages/Main';
 import useAuthStore from './stores/userStore';
 
 export default function App() {
-  const { getAccessToken, getRefreshToken, login, logout } = useAuthStore();
 
   useEffect(() => {
-    const trySilentLogin = async () => {
-      const accessToken = getAccessToken();
-      //const refreshToken = getRefreshToken();
-      let refreshToken;
-      try {
-        const storageData = sessionStorage.getItem('auth-storage');
-        if (storageData) {
-          console.log(storageData);
-          const authData = JSON.parse(storageData);
-          refreshToken = authData.state.tokens.refreshToken;
-          console.log(refreshToken);
-        } else {
-          refreshToken = null;
+    const initializeAuth = async () => {
+      const authStore = useAuthStore.getState();
+
+      
+      if(authStore.isAuthenticated && authStore.getRefreshToken()){
+        if (!authStore.getAccessToken()) {
+          console.log("페이지 새로고침 감지: 액세스 토큰 복구 시도");
+          await authStore.refreshAccessToken();
         }
-      } catch (e) {
-        console.error("세션 스토리지 읽기 오류:", e);
-        refreshToken = null;
       }
-      
-      console.log("세션 스토리지에서 직접 읽은 리프레시 토큰:", refreshToken);
-
-
-      if (accessToken) {
-        console.log("이미 로그인 되어 있음", accessToken);
-        return;
-      }
-      
-      if (!refreshToken) {
-        console.log("로그인 정보 없음");
-        return;
-      }
-
-      try {
-        const res = await axios.post("/api/auth/refresh", {
-          refreshToken,
-        });
-
-        console.log(res);
-        const { accessToken, refreshToken: newRefreshToken, refreshTokenExpiresIn, user } = res.data;
-
-        login({
-          refreshToken: newRefreshToken,
-          refreshTokenExpiresIn,
-          user,
-        });
-      } catch (err) {
-        // refreshToken도 만료되었으면 로그아웃
-        logout();
-      }
-    };
-    trySilentLogin();
+    }
+    initializeAuth();
   }, []);
 
 
