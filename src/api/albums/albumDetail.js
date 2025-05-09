7ac46af81c1f1015f08448f6bf91ef7528b413de
zwen-yirochi <jijohn.vega@gmail.com>
2025-05-09 13:50:36 +0900

    feat:앨범 상세 조회 api 수정

--- src/api/albums/albumDetail.js
+++ src/api/albums/albumDetail.js
@@ -1,28 +1,14 @@
-import useAuthStore from "../../stores/userStore";
+import { authenticatedFetch } from "../auth/authUtils";
 import { API_BASE_URL } from "../config";
 
 
 const getAlbumDetail = async (albumId) => {
-    const accessToken = useAuthStore.getState().getAccessToken();
     try {
         const apiUrl = API_BASE_URL + `/api/album/${albumId}`;
-
-        const requestOptions = {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': `Bearer ${accessToken}`
-            }
-        };
-        const response = await fetch(apiUrl, requestOptions);
-
-        if (!response.ok) {
-            const errorData = await response.json().catch(() => ({}));
-            throw new Error(errorData.message || `HTTP 에러! 상태: ${response.status}`);
-        }
-        return await response.json();
+        return await authenticatedFetch(apiUrl, { method: 'GET' });
     } catch (error) {
-        console.error('앨범 상세 정보 실패:', error.message);
+        console.error('앨범 상세 데이터 Fetch 실패:', error.message);
+        throw error;
     }
 
 }
