dc5d03409fd1fdb6d5f18ff8f27d0d314c036359
zwen-yirochi <jijohn.vega@gmail.com>
2025-05-09 12:50:26 +0900

    feat:스타일 수정, 배경화면 추가 및 부작용 제어

--- src/components/AlbumListHeader.jsx
+++ src/components/AlbumListHeader.jsx
@@ -5,7 +5,7 @@ import locationIcon from "../assets/icons/location_icon.png";
 
 const AlbumListHeader = () => {
 	return (
-		<div className="h-[48px] relative rounded-t-2xl border-t-2 border-solid mb-8">
+		<div className="h-[48px] relative border-t-2 border-solid mb-8">
 			<div className="p-4">
 				<div className="absolute flex flex-row gap-2">
 					<img className="size-[24px]" src={albumIcon}></img>
