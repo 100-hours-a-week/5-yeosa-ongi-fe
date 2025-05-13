bf6451e79501fe01628d6aa165098f4334822c1c
zwen-yirochi <jijohn.vega@gmail.com>
2025-05-13 15:38:52 +0900

    feat:앨범 생성 버튼 스타일 변경

--- src/components/album/CreateAlbumButton.jsx
+++ src/components/album/CreateAlbumButton.jsx
@@ -1,23 +1,24 @@
 const CreateAlbumButton = ({ disabled, onClick }) => (
-  <button
-    className={`
-      fixed left-0 right-0 bottom-0
-      mx-4 mb-4
-      h-14 w-auto
-      text-lg font-bold z-50
-      rounded-xl
-      shadow-lg
-      ${!disabled
-        ? 'bg-primary text-white'
-        : 'bg-gray-200 text-gray-400 cursor-not-allowed'
-      }
-    `}
-    style={{ maxWidth: "calc(100vw - 2rem)" }} // 혹시 너무 넓어질 때 대비
-    disabled={disabled}
-    onClick={onClick}
-  >
-    앨범 생성하기
-  </button>
+	<div className="fixed bottom-0 left-0 right-0 flex justify-center w-full mb-4">
+		<div className="w-full max-w-[528px] px-4">
+			<button
+				className={`
+          h-14 w-full
+          text-lg font-bold z-50
+          rounded-xl
+          shadow-lg
+          ${
+				!disabled
+					? "bg-primaryBold text-white"
+					: "bg-gray-200 text-gray-400 cursor-not-allowed"
+			}
+        `}
+				disabled={disabled}
+				onClick={onClick}>
+				앨범 생성하기
+			</button>
+		</div>
+	</div>
 );
 
 export default CreateAlbumButton;
