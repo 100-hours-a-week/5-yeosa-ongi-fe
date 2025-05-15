31039ef896ba1e0a6bb69fc36f72c194a2f7f2bc
zwen-yirochi <jijohn.vega@gmail.com>
2025-05-16 02:35:49 +0900

    feat:헤더 버튼 숨김 기능 추가.

--- src/pages/MyActivities.jsx
+++ src/pages/MyActivities.jsx
@@ -1,19 +1,23 @@
 import { useNavigate } from "react-router-dom";
+import Header from "../components/common/Header";
 
 const MyActivities = () => {
 	const navigate = useNavigate();
 
 	return (
-		<div className="flex flex-col items-center justify-center min-h-screen p-6 bg-gray-50">
-			<div className="w-full max-w-md p-8 text-center bg-white rounded-lg shadow-md">
-				<div onClick={() => navigate(-1)} className="mb-4 text-5xl">
-					🚧
+		<>
+			<Header showButtons={false} />
+			<div className="flex flex-col items-center justify-center h-full p-6 bg-gray-50">
+				<div className="w-full max-w-md p-8 text-center bg-white rounded-lg shadow-md">
+					<div onClick={() => navigate(-1)} className="mb-4 text-5xl">
+						🚧
+					</div>
+					<p className="text-gray-600">
+						내 활동 페이지는 현재 개발 중입니다.
+					</p>
 				</div>
-				<p className="text-gray-600">
-					내 활동 페이지는 현재 개발 중입니다.
-				</p>
 			</div>
-		</div>
+		</>
 	);
 };
 
