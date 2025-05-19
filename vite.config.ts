ce267dd7e590496f978e67929a9a447dbb4ff7fa
YuSung011017 <salamander011017@gmail.com>
2025-05-19 13:51:28 +0900

    fix : vite.config.ts 수정

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,18 @@
-import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
       '@': '/src',
     },
+  },
+  server: {
+    host: true, // 외부 접속 허용 (0.0.0.0)
+    port: 5173, // 기본 포트
+    allowedHosts: ['ongi.today'], // 🔥 여기 추가
   },
   build: {
     minify: 'terser',
