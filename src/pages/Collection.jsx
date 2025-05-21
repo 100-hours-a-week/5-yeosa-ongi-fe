b5ad80e37415116bbe5525ae21700d615b2d15fe
zwen-yirochi <jijohn.vega@gmail.com>
2025-05-21 15:30:09 +0900

    feat:버튼 레이아웃 개선

--- src/pages/Collection.jsx
+++ src/pages/Collection.jsx
@@ -209,31 +209,32 @@ const Collection = () => {
 					{currentCollection.name || "컬렉션"}
 				</div>
 			</div>
-			<div className="flex items-center justify-between p-4">
+			<div className="flex items-center justify-between h-10 m-4">
 				<p className="text-sm text-gray-dark">
 					총 {currentCollection.count || pictures.length}개의 사진
 				</p>
+
 				{isSelectMode ? (
 					<button
 						onClick={isRecovery ? handleRecoverClick : handleClick}>
 						<div className="text-sm">완료</div>
 					</button>
 				) : isCollectionShaky() ? (
-					<>
+					<div className="flex items-center justify-between gap-4">
 						<button
 							onClick={() => {
 								setIsRecovery(true);
 								setIsSelectMode(true);
 							}}>
-							<img src={iconRecovery} className="h-6"></img>
+							<img src={iconRecovery} className="h-6 "></img>
 						</button>
 						<button
 							onClick={() => {
 								setIsSelectMode(true);
 							}}>
-							<img src={iconTrash} className="h-4"></img>
+							<img src={iconTrash} className="h-6"></img>
 						</button>
-					</>
+					</div>
 				) : (
 					<button
 						onClick={() => {
