960f9851fd2fd3fbaab2dcc8938ca46cff7fd217
zwen-yirochi <jijohn.vega@gmail.com>
2025-05-22 16:16:37 +0900

    feat:모달 프레임 닫기 버튼 스타일 수정- 밑줄 제거

--- src/components/common/Modal.jsx
+++ src/components/common/Modal.jsx
@@ -17,9 +17,9 @@ export function Modal({ isOpen, onClose, title, children }) {
 			<div
 				className="relative w-full max-w-md mx-auto overflow-hidden transition-all transform bg-white rounded-lg shadow-xl dark:bg-gray-800"
 				onClick={(e) => e.stopPropagation()}>
-				<div className="absolute right-0 z-20 p-4 border-b border-gray-200  dark:border-gray-700">
+				<div className="absolute right-0 z-20 p-4 ">
 					<button
-						className="p-1 text-gray-500 transition-colors rounded-full  hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-700"
+						className="p-1 text-gray-500 transition-colors rounded-full hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-700"
 						onClick={onClose}>
 						<svg
 							className="w-6 h-6"
