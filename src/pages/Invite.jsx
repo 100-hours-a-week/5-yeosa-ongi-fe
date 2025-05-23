f0c48b76ce513791d1ba502363207eb67505de59
zwen-yirochi <jijohn.vega@gmail.com>
2025-05-23 14:28:08 +0900

    Feature/#127 (#138)
    * fix:vite.config 수정
    
    * feat:초대 페이지 라우팅 및 버튼 구성
    
    * feat:초대 로직 수정
    
    * feat:공유 링크 생성 위치 수정
    
    * chore:git ignore SSH 키 관련 업데이트
    
    * feat:앨범 권한 확인 api 구현

--- src/pages/Invite.jsx
+++ src/pages/Invite.jsx
@@ -1,12 +1,27 @@
 
-import { useSearchParams } from "react-router-dom";
+import { useNavigate, useSearchParams } from "react-router-dom";
+import { getAlbumAccess } from "../api/albums/albumAccessApi";
+
 import { comfirmInvite } from "../api/albums/inviteUser";
 
 const Invite = () => {
 	const [searchParams] = useSearchParams();
 	const token = searchParams.get("token");
+
+	const navigate = useNavigate();
+
 	const handleClick = () => {
-		comfirmInvite(token);
+		const proccessInvite = async () => {
+			const response = await comfirmInvite(token);
+
+			const albumId = response.data.albumId;
+			const result = await getAlbumAccess(albumId);
+			if (result.data.role) {
+				navigate(`/album/${albumId}`);
+			}
+		};
+		proccessInvite();
+
 	};
 
 	return (
@@ -20,7 +35,6 @@ const Invite = () => {
           rounded-xl
           shadow-lg
 		  bg-primary
-
         `}
 						onClick={handleClick}>
 
