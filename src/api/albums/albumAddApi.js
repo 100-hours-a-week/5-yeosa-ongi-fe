d016ad304c9cd456c72f19b579a8ee66038471bf
zwen-yirochi <jijohn.vega@gmail.com>
2025-05-23 14:50:40 +0900

    feat:앨범에 사진 추가하는 api 구현

---
+++ src/api/albums/albumAddApi.js
@@ -0,0 +1,15 @@
+import { authenticatedFetch } from "../auth/authUtils";
+import { API_BASE_URL } from "../config";
+
+const addAlbumPicture = async (albumId, albumData) => {
+    try {
+        const apiUrl = API_BASE_URL + `/api/album/${albumId}`;
+        return await authenticatedFetch(apiUrl, { method: 'POST', body: JSON.stringify(albumData) });
+    } catch (error) {
+        console.error('앨범 사진 추가 실패:', error.message);
+        throw error;
+    }
+
+}
+
+export { addAlbumPicture };
