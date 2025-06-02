5eac4993bc9de7469f17101865cfed6fac5ed5ff
zwen-yirochi <jijohn.vega@gmail.com>
2025-06-02 13:09:08 +0900

    chore:Typescript 설정 및 포맷팅 설정

--- eslint.config.js
+++ eslint.config.js
@@ -1,31 +1,59 @@
 import js from '@eslint/js'
+import prettier from 'eslint-config-prettier'
+import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import globals from 'globals'
 import tseslint from 'typescript-eslint'
 
 export default tseslint.config(
-  { ignores: ['dist'] },
+  { ignores: ['dist', 'node_modules'] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx,js,jsx}'],
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommended,
+      prettier, // Prettier와 충돌하는 규칙 비활성화
+    ],
+    files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+      parserOptions: {
+        project: ['./tsconfig.node.json', './tsconfig.app.json'],
+        tsconfigRootDir: import.meta.dirname,
+      },
     },
     plugins: {
+      'react': react,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
+      ...react.configs.recommended.rules,
+      ...react.configs['jsx-runtime'].rules,
+
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
       ],
-      "no-unused-vars": "on",
-      " react/prop-types": "off",
-      "@typescript-eslint/no-unused-vars": "error",
+
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+        }
+      ],
+
+      'react/prop-types': 'off',
+      'react/react-in-jsx-scope': 'off',
+      'no-unused-vars': 'off',
     },
-  },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+  }
 )
