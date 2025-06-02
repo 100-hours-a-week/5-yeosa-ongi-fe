16e2e261035d5cff1443d903654886a55466fc0d
zwen-yirochi <jijohn.vega@gmail.com>
2025-06-02 14:47:41 +0900

    chore:eslint 미사용 변수 에러 설정 해제

--- eslint.config.js
+++ eslint.config.js
@@ -7,53 +7,53 @@ import globals from 'globals'
 import tseslint from 'typescript-eslint'
 
 export default tseslint.config(
-  { ignores: ['dist', 'node_modules'] },
-  {
-    extends: [
-      js.configs.recommended,
-      ...tseslint.configs.recommended,
-      prettier, // Prettier와 충돌하는 규칙 비활성화
-    ],
-    files: ['**/*.{ts,tsx}'],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-      parserOptions: {
-        project: ['./tsconfig.node.json', './tsconfig.app.json'],
-        tsconfigRootDir: import.meta.dirname,
-      },
-    },
-    plugins: {
-      'react': react,
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-    },
-    rules: {
-      ...reactHooks.configs.recommended.rules,
-      ...react.configs.recommended.rules,
-      ...react.configs['jsx-runtime'].rules,
+    { ignores: ['dist', 'node_modules'] },
+    {
+        extends: [
+            js.configs.recommended,
+            ...tseslint.configs.recommended,
+            prettier, // Prettier와 충돌하는 규칙 비활성화
+        ],
+        files: ['**/*.{ts,tsx}'],
+        languageOptions: {
+            ecmaVersion: 2020,
+            globals: globals.browser,
+            parserOptions: {
+                project: ['./tsconfig.node.json', './tsconfig.app.json'],
+                tsconfigRootDir: import.meta.dirname,
+            },
+        },
+        plugins: {
+            'react': react,
+            'react-hooks': reactHooks,
+            'react-refresh': reactRefresh,
+        },
+        rules: {
+            ...reactHooks.configs.recommended.rules,
+            ...react.configs.recommended.rules,
+            ...react.configs['jsx-runtime'].rules,
 
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
+            'react-refresh/only-export-components': [
+                'warn',
+                { allowConstantExport: true },
+            ],
 
-      '@typescript-eslint/no-unused-vars': [
-        'error',
-        {
-          argsIgnorePattern: '^_',
-          varsIgnorePattern: '^_',
-        }
-      ],
+            '@typescript-eslint/no-unused-vars': [
+                'error',
+                {
+                    argsIgnorePattern: '^_',
+                    varsIgnorePattern: '^_',
+                }
+            ],
 
-      'react/prop-types': 'off',
-      'react/react-in-jsx-scope': 'off',
-      'no-unused-vars': 'off',
-    },
-    settings: {
-      react: {
-        version: 'detect',
-      },
-    },
-  }
+            'react/prop-types': 'off',
+            'react/react-in-jsx-scope': 'off',
+            'no-unused-vars': 'on',
+        },
+        settings: {
+            react: {
+                version: 'detect',
+            },
+        },
+    }
 )
