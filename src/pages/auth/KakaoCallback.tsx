c3c254ae0c59a8d1078c05c473a9bb49f6cee1a9
zwen-yirochi <jijohn.vega@gmail.com>
2025-06-04 14:44:04 +0900

    refactor:콜백 컴포넌트 ts 변환

--- src/pages/auth/KakaoCallback.tsx
+++ src/pages/auth/KakaoCallback.tsx
@@ -1,17 +1,27 @@
-// KakaoCallback.js - 리다이렉트 처리 컴포넌트
 import { useEffect, useState } from 'react'
 import { useLocation, useNavigate, useSearchParams } from 'react-router-dom'
-import { kakaoLogin } from '../../api/auth/login'
+
+// Components
 import MovingDotsLoader from '../../components/common/MovingDotsLoader'
+
+//APIs
+import { kakaoLogin } from '../../api/auth'
+
+// Store
 import useUserStore from '../../stores/userStore'
 
+interface InviteData {
+    type: 'invite'
+    redirectUrl: string
+}
+
 const KakaoCallback = () => {
-    console.log('카카오 콜백')
     const [searchParams] = useSearchParams()
     const location = useLocation()
     const navigate = useNavigate()
-    const [loading, setLoading] = useState(true)
-    const [error, setError] = useState('')
+
+    const [loading, setLoading] = useState<boolean>(true)
+    const [error, setError] = useState<string>('')
 
     const login = useUserStore(state => state.login)
 
@@ -66,11 +76,9 @@ const KakaoCallback = () => {
     if (loading) {
         return <MovingDotsLoader />
     }
-
     if (error) {
         return <div className='self-auto'>오류: {error}</div>
     }
-
     return null
 }
 
