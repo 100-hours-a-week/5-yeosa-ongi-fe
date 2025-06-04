1f9bf023d4e40b0c820bd8429e3c5b48035f14b5
zwen-yirochi <jijohn.vega@gmail.com>
2025-06-05 04:31:40 +0900

    style:버튼 높이 조정

--- src/components/AlbumEditor/AlbumEditorHeader.jsx
+++ src/components/AlbumEditor/AlbumEditorHeader.jsx
@@ -5,11 +5,8 @@ const AlbumEditorHeader = ({ title }) => {
     return (
         <>
             <header className='h-[52px] relative flex items-center justify-center'>
-                <button
-                    className='absolute left-4 top-1/4'
-                    onClick={() => navigate(-1)}
-                >
-                    <img className='h-1/2' src={Arrow_Left} alt='뒤로가기' />
+                <button className='absolute left-4 top-1/4' onClick={() => navigate(-1)}>
+                    <img className='h-4' src={Arrow_Left} alt='뒤로가기' />
                 </button>
                 <h1 className='text-center'>{title}</h1>
             </header>
