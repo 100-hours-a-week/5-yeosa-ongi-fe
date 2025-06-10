46f7a55350ad757b4facd0b0fbb393e12c9f304a
zwen-yirochi <jijohn.vega@gmail.com>
2025-06-10 15:13:58 +0900

    feat:getter함수 추가

--- src/stores/mainPageStore.jsx
+++ src/stores/mainPageStore.jsx
@@ -1,11 +1,14 @@
 import { create } from 'zustand'
 
-const useMainPageStore = create(set => ({
+const useMainPageStore = create((set, get) => ({
     selectedId: null, // 현재 선택된 항목의 ID
     selectedAlbumSummary: null,
     // 항목 선택 함수
     setSelectedAlbumSummary: data => set({ selectedAlbumSummary: data }),
     selectItem: id => set({ selectedId: id }),
+    // getter 함수들
+    getSelectedId: () => get().selectedId,
+    getSelectedAlbumSummary: () => get().selectedAlbumSummary,
     // 선택 취소 함수
     clearSelection: () => set({ selectedId: null }),
 }))
