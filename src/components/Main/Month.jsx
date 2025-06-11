0323f893d33a830f9672005b29ca3f92db5ddea0
zwen-yirochi <jijohn.vega@gmail.com>
2025-06-11 09:45:54 +0900

    feat:Month 컴포넌트에  Grid 바깥 영역 클릭 로직 구현

--- src/components/Main/Month.jsx
+++ src/components/Main/Month.jsx
@@ -1,15 +1,36 @@
 import Grid from '@/components/common/Grid'
+import { useEffect, useRef } from 'react'
 import AlbumThumbnail from './AlbumThumbnail'
 
 const Month = ({ albumIds, title }) => {
+    const gridRef = useRef(null)
     let items = []
     albumIds.forEach(id => {
         items.push({ ElementType: AlbumThumbnail, element: id, id })
     })
+    useEffect(() => {
+        const handleClickOutside = event => {
+            if (gridRef.current && !gridRef.current.contains(event.target)) {
+                console.log('Grid 외부 클릭됨')
+                // 원하는 동작 실행
+                handleOutsideClick()
+            }
+        }
+
+        document.addEventListener('mousedown', handleClickOutside)
+        return () => document.removeEventListener('mousedown', handleClickOutside)
+    }, [])
+
+    const handleOutsideClick = () => {
+        // Grid 외부 클릭 시 실행할 동작
+        console.log('Grid 바깥 영역 클릭!')
+    }
     return (
         <>
             <div className='h-8 p-1 ml-2 text-md'>{title}</div>
-            <Grid items={items}></Grid>
+            <div ref={gridRef}>
+                <Grid items={items}></Grid>
+            </div>
         </>
     )
 }
