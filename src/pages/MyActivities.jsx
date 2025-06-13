9e4a9adad09ebe108343158df0fa8a4941795a27
zwen-yirochi <jijohn.vega@gmail.com>
2025-06-13 14:02:32 +0900

    feat:Intro, MostTag 컴포넌트 구현

--- src/pages/MyActivities.jsx
+++ src/pages/MyActivities.jsx
@@ -3,6 +3,8 @@ import { useNavigate } from 'react-router-dom'
 // Components
 import IncomeChart from '@/components/MyPage/Chart'
 import StorageWidget from '@/components/MyPage/ChartBar'
+import Intro from '@/components/MyPage/Intro'
+import MostTag from '@/components/MyPage/MostTag'
 import Header from '../components/common/Header'
 
 const MyActivities = () => {
@@ -11,8 +13,10 @@ const MyActivities = () => {
     return (
         <>
             <Header showButtons={false} />
+            <Intro />
             <StorageWidget />
             <IncomeChart />
+            <MostTag />
         </>
     )
 }
