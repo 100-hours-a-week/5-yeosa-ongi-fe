5f87ad0581a4fa719847705bf7156ffb36734658
zwen-yirochi <jijohn.vega@gmail.com>
2025-06-16 22:12:01 +0900

    feat:AlbumTitle 컴포넌트에 기능 추가

--- src/components/Album/AlbumTitle.jsx
+++ src/components/Album/AlbumTitle.jsx
@@ -1,5 +1,30 @@
+import { changeAlbumName } from '@/api/album'
+import { useParams } from 'react-router-dom'
+import EditableText from '../common/EditableText'
+
 const AlbumTitle = ({ title }) => {
-    return <div className='mt-2 ml-4 font-sans text-lg'>{title}</div>
+    const { albumId } = useParams()
+    const handleSaveName = async newValue => {
+        // 서버 저장 시뮬레이션
+        try {
+            const response = await changeAlbumName(albumId, newValue)
+        } catch (error) {
+            console.error(error)
+        }
+    }
+
+    return (
+        <div className='p-4'>
+            <EditableText
+                value={title}
+                onSave={handleSaveName}
+                textFieldProps={{
+                    placeholder: '이름을 입력하세요',
+                    required: true,
+                }}
+            />
+        </div>
+    )
 }
 
 export default AlbumTitle
