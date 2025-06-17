8af9c8452a7c59f6b8663b3a43a93f0e08dd12cc
zwen-yirochi <jijohn.vega@gmail.com>
2025-06-17 11:17:00 +0900

    refactor:파일 확장자 tsx로 변경 및 props 타입  정의

---
+++ src/components/AlbumEditor/AlbumEditorHeader.tsx
@@ -0,0 +1,24 @@
+import { useNavigate } from 'react-router-dom'
+
+// Assets
+import Arrow_Left from '@/assets/icons/Arrow_Left.png'
+
+interface AlbumEditorHeaderProps {
+    title: string
+}
+
+const AlbumEditorHeader = ({ title }: AlbumEditorHeaderProps) => {
+    const navigate = useNavigate()
+    return (
+        <>
+            <header className='h-[52px] relative flex items-center justify-center'>
+                <button className='absolute left-4 top-1/4' onClick={() => navigate(-1)}>
+                    <img className='h-4' src={Arrow_Left} alt='뒤로가기' />
+                </button>
+                <h1 className='text-center'>{title}</h1>
+            </header>
+        </>
+    )
+}
+
+export default AlbumEditorHeader
