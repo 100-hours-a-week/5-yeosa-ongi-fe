b472055f34647e66aa628ddcb240671716d56286
zwen-yirochi <jijohn.vega@gmail.com>
2025-06-18 16:10:18 +0900

    refactor:OnboardingScreen tsx파일로 변환

--- src/components/Main/OnboardingScreen.tsx
+++ src/components/Main/OnboardingScreen.tsx
@@ -25,13 +25,7 @@ const OnboardingScreen = () => {
             >
                 <div className='relative flex items-center justify-center w-56 h-56'>
                     {/* 점선 원형 테두리 */}
-                    <div
-                        className='absolute inset-0 bg-white border-2 rounded-full 2border-gray-300 animate-pulse'
-                        style={{
-                            borderStyle: 'dashed',
-                            borderDashArray: '8 20',
-                        }}
-                    />
+                    <div className='absolute inset-0 bg-white border-2 border-gray-300 border-dashed rounded-full animate-pulse' />
                 </div>
             </div>
         </div>
