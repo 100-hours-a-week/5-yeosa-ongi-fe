550a8975d2d9ea4948849bdbf3e5d0109bd3ed0f
zwen-yirochi <jijohn.vega@gmail.com>
2025-06-19 16:03:52 +0900

    refactor:getCoworkerList API props 수정

--- src/api/album.ts
+++ src/api/album.ts
@@ -124,7 +124,7 @@ export const getAlbumDetail = async (albumId: string): Promise<ApiResponse> => {
  * @param albumId
  * @returns
  */
-export const getCoworkersList = async (albumId: number): Promise<ApiResponse> => {
+export const getCoworkersList = async (albumId: string): Promise<ApiResponse> => {
     try {
         const apiUrl = API_BASE_URL + `/api/album/${albumId}/members`
         return await authenticatedFetch(apiUrl, { method: 'GET' })
