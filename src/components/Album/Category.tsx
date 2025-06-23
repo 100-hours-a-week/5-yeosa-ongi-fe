7a4f30374b9c2174b4ddae5dc9fc59ab3da3faa9
zwen-yirochi <jijohn.vega@gmail.com>
2025-06-23 15:42:47 +0900

    fix:RawPicture  사진 url의 키값 pictureURL 로 변경

--- src/components/Album/Category.tsx
+++ src/components/Album/Category.tsx
@@ -10,7 +10,6 @@ interface CategoryProps {
 
 const Category = ({ pictures, title, albumId }: CategoryProps) => {
     const navigate = useNavigate()
-
     const renderDefaultThumbnail = (): HTMLElement => {
         const placeholder = document.createElement('div')
         placeholder.className = 'default-thumbnail'
@@ -22,10 +21,10 @@ const Category = ({ pictures, title, albumId }: CategoryProps) => {
         <div className='w-[104px] h-[128px] min-w-[120px] bg-gray-light rounded-md overflow-hidden shadow-sm flex flex-col items-center p-2 pb-0'>
             <button onClick={() => navigate(`/album/${albumId}/${title}`)}>
                 {/* 이미지 부분 */}
-                {pictures && pictures.length > 0 && pictures[0].url ? (
+                {pictures && pictures.length > 0 && pictures[0].pictureURL ? (
                     <div className='size-[96px] w-[100px] border border-gray-300 overflow-hidden'>
                         <img
-                            src={pictures[0].url}
+                            src={pictures[0].pictureURL}
                             alt={title}
                             className='object-cover w-full h-full'
                             onError={(e: React.SyntheticEvent<HTMLImageElement, Event>) => {
