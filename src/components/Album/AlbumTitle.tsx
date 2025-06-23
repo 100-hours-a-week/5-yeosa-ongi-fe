80f28f7e6459c9f303c004b4014f6ad34336fa0a
zwen-yirochi <jijohn.vega@gmail.com>
2025-06-23 16:43:05 +0900

    fix:앨범 제목 validator 추가 후 TextField에게 전달

--- src/components/Album/AlbumTitle.tsx
+++ src/components/Album/AlbumTitle.tsx
@@ -1,16 +1,30 @@
 import { changeAlbumName } from '@/api/album'
+import { useState } from 'react'
 import { useParams } from 'react-router-dom'
 import EditableText from '../common/EditableText'
 
+const validator = (value: string) => {
+    if (value === '') {
+        return '제목을 입력하세요'
+    } else if (value.length > 12) {
+        return '12글자 이하'
+    } else {
+        return true
+    }
+}
 interface AlbumTitleProps {
     title: string
 }
 
 const AlbumTitle = ({ title }: AlbumTitleProps) => {
     const { albumId } = useParams()
+    const [currentTitle, setCurrentTitle] = useState<string>(title)
+
     const handleSaveName = async (newValue: string) => {
         try {
             const response = await changeAlbumName(albumId as string, newValue)
+            // 성공하면 로컬 상태 업데이트
+            setCurrentTitle(newValue)
         } catch (error) {
             console.error(error)
         }
@@ -19,11 +33,12 @@ const AlbumTitle = ({ title }: AlbumTitleProps) => {
     return (
         <div className='p-4'>
             <EditableText
-                value={title}
+                value={currentTitle}
                 onSave={handleSaveName}
                 textFieldProps={{
                     placeholder: '이름을 입력하세요',
                     required: true,
+                    validator: validator,
                 }}
             />
         </div>
