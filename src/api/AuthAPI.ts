3c0a2b2c9ddbd3defb35abbb44b069f1165f9af7
zwen-yirochi <jijohn.vega@gmail.com>
2025-06-28 19:04:23 +0900

    feat:AuthAPI 구현

---
+++ src/api/AuthAPI.ts
@@ -0,0 +1,23 @@
+import { APIResponse, User } from '@/types/api.types'
+import APIBuilder from './core/APIBuilder'
+
+export class AuthAPI {
+    // 로그인
+    static login() {
+        return APIBuilder.get(`/api/auth`).build().call<APIResponse>()
+    }
+
+    static kakaoLogin(authorization_code: string) {
+        return APIBuilder.get(`/auth/login/kakao?code=${authorization_code}`).build().call<APIResponse<User>>()
+    }
+
+    // Access Token 재발급
+    static refreshAuth(refreshToken: string) {
+        return APIBuilder.post(`/api/auth/refresh`, { refreshToken }).build().call<APIResponse>
+    }
+
+    // 로그아웃
+    static logout(refreshToken: string) {
+        return APIBuilder.post(`/api/auth`, { refreshToken }).build().call<APIResponse>
+    }
+}
