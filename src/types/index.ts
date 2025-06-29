869f70ea8eccbdcd6939d654198bd78ce2b9f49a
zwen-yirochi <jijohn.vega@gmail.com>
2025-06-29 22:16:18 +0900

    refactor:미사용 타입 제거

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,13 +1,5 @@
 import { FC } from 'react'
 
-export interface ApiResponse<T = any> {
-    success: boolean
-    data?: T
-    message?: string
-    error?: string
-    code?: string
-}
-
 export interface GridItem {
     ElementType: FC<any> | React.ComponentType<any>
     element: number
