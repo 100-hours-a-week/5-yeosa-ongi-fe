315ad906da89843c9d29b3577b0458964f969a93
zwen-yirochi <jijohn.vega@gmail.com>
2025-06-30 14:20:51 +0900

    feat:z index 수정

--- src/components/common/FlottingButton.tsx
+++ src/components/common/FlottingButton.tsx
@@ -8,7 +8,7 @@ const FlottingButton = ({ albumId }: FlottingButtonProps) => {
         window.location.href = `/album-editor/${location}`
     }
     return (
-        <div className='absolute z-48 right-12 size-4 bottom-16'>
+        <div className='absolute z-40 right-12 size-4 bottom-16'>
             <button
                 className='flex items-center justify-center text-2xl text-white transition-colors rounded-full shadow-lg bg-primary w-14 h-14 hover:bg-secondary focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-50'
                 onClick={handleClick}
