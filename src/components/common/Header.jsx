e0030a1b2bf8c167c172aa9bc60f9e935ffb38bf
zwen-yirochi <jijohn.vega@gmail.com>
2025-07-02 00:23:37 +0900

    feat:알림 버튼 제거

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -2,7 +2,6 @@ import { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import useAuthStore from '../../stores/authStore'
 import defaultProfileImage from '/src/assets/default_user_imgae.png'
-import bellIcon from '/src/assets/icons/bell_icon.png'
 import ongiLogoFlat from '/src/assets/ongi_logo_flat.png'
 
 const Header = ({ showButtons = true }) => {
@@ -54,13 +53,6 @@ const Header = ({ showButtons = true }) => {
             <div className='w-40'></div>
             {showButtons && (
                 <div className='flex items-center space-x-0'>
-                    <button
-                        className='flex items-center justify-center p-1 mt-2 transition-colors rounded-full size-8 hover:bg-gray-100 min-w-6'
-                        aria-label='Notifications'
-                        onClick={() => navigate('/notification')}
-                    >
-                        <img className='h-4 md:h-7' src={bellIcon} alt='Notifications' />
-                    </button>
                     <button
                         className='p-1 transition-colors rounded-full size-8 hover:bg-gray-100'
                         aria-label='Profile'
