3088fc5759697b158df041d2ab4dc65637c7cf58
zwen-yirochi <jijohn.vega@gmail.com>
2025-07-02 00:29:03 +0900

    feat:숫자 표시되지 않는 오류 해결 및 커뮤니티 버튼 제거

--- src/components/Main/AlbumListHeader.tsx
+++ src/components/Main/AlbumListHeader.tsx
@@ -3,7 +3,6 @@ import { useNavigate } from 'react-router-dom'
 // APIs
 
 //Assets
-import communityIcon from '@/assets/icons/community_icon.png'
 import albumIcon from '@/assets/icons/images_icon.png'
 import locationIcon from '@/assets/icons/location_icon.png'
 
@@ -26,20 +25,11 @@ const AlbumListHeader = () => {
                 <div className='p-2'>
                     <div className='absolute flex flex-row items-center gap-2 '>
                         <img className='size-4' src={albumIcon} alt='Album icon' />
-                        <div className='text-sm'>{data?.totalPhotos}</div>
+                        <div className='text-sm'>{data?.albumCount}</div>
                     </div>
                     <div className='absolute flex flex-row items-center gap-2 left-1/4'>
                         <img className='size-4' src={locationIcon} alt='Location icon' />
-                        <div className='text-sm '>{data?.totalPlaces}</div>
-                    </div>
-                    <div>
-                        <button
-                            className='absolute flex flex-row items-center gap-1 right-4'
-                            onClick={() => navigate('/community')}
-                        >
-                            <div className='text-sm text-primary'>community</div>
-                            <img className='size-4' src={communityIcon} alt='Community icon' />
-                        </button>
+                        <div className='text-sm '>{data?.placeCount}</div>
                     </div>
                 </div>
             </div>
