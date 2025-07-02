37a9d208309f63f4b3da6025900ae5e46ea2f21d
zwen-yirochi <jijohn.vega@gmail.com>
2025-07-02 11:17:23 +0900

    feat:미사용 API 제거

--- src/api/album.ts
+++ src/api/album.ts
@@ -3,21 +3,6 @@ import { authenticatedFetch } from './authUtils'
 import { API_BASE_URL } from './config'
 
 /**
- * 앨범 권한 확인
- * @param albumId
- * @returns
- */
-export const getAlbumAccess = async (albumId: string): Promise<APIResponse> => {
-    try {
-        const apiUrl = API_BASE_URL + `/api/album/${albumId}/role`
-        return await authenticatedFetch(apiUrl, { method: 'GET' })
-    } catch (error) {
-        console.error('앨범 권한 확인 실패:', (error as Error).message)
-        throw error
-    }
-}
-
-/**
  * 앨범 사진 추가
  * @param albumId: number
  * @param albumData
@@ -32,19 +17,6 @@ export const addAlbumPicture = async (albumId: number, albumData: any): Promise<
         })
     } catch (error) {
         console.error('앨범 사진 추가 실패:', (error as Error).message)
-        throw error
-    }
-}
-
-export const changeAlbumName = async (albumId: string, albumName: string) => {
-    try {
-        const apiUrl = API_BASE_URL + `/api/album/${albumId}`
-        return await authenticatedFetch(apiUrl, {
-            method: 'PUT',
-            body: JSON.stringify({ albumName: albumName }),
-        })
-    } catch (error) {
-        console.error('앨범 이름 수정 실패:', (error as Error).message)
         throw error
     }
 }
@@ -67,20 +39,7 @@ export const createAlbum = async (data: any): Promise<APIResponse> => {
     }
 }
 
-/**
- * 앨범 삭제
- * @param albumId
- * @returns
- */
-export const deleteAlbum = async (albumId: string): Promise<APIResponse> => {
-    try {
-        const apiUrl = API_BASE_URL + `/api/album/${albumId}`
-        return await authenticatedFetch(apiUrl, { method: 'DELETE' })
-    } catch (error) {
-        console.error('앨범 삭제 요청 실패:', (error as Error).message)
-        throw error
-    }
-}
+//==== = = == = = = == = = = = =
 
 export const fetchAlbumData = async (yearMonth: string): Promise<APIResponse> => {
     try {
@@ -88,16 +47,6 @@ export const fetchAlbumData = async (yearMonth: string): Promise<APIResponse> =>
         return await authenticatedFetch(apiUrl, { method: 'GET' })
     } catch (error) {
         console.error('앨범 데이터 Fetch 실패:', (error as Error).message)
-        throw error
-    }
-}
-
-export const getAlubmSummary = async (albumId: string): Promise<APIResponse> => {
-    try {
-        const apiUrl = API_BASE_URL + `/api/album/${albumId}/summary`
-        return await authenticatedFetch(apiUrl, { method: 'GET' })
-    } catch (error) {
-        console.error('앨범 요약 정보 조회 요청 실패:', error as Error)
         throw error
     }
 }
@@ -135,21 +84,6 @@ export const getCoworkersList = async (albumId: string): Promise<APIResponse> =>
 }
 
 /**
- * 공동 작업자 초대 링크 생성
- * @param albumId
- * @returns
- */
-export const getSharingLink = async (albumId: string): Promise<APIResponse> => {
-    try {
-        const apiUrl = API_BASE_URL + `/api/album/${albumId}/invite/link`
-        return await authenticatedFetch(apiUrl, { method: 'POST' })
-    } catch (error) {
-        console.error('앨범 공유 링크 생성 요청 실패:', (error as Error).message)
-        throw error
-    }
-}
-
-/**
  * 공동 작업자 추가
  * @param inviteToken
  * @returns
@@ -160,22 +94,6 @@ export const comfirmInvite = async (inviteToken: string): Promise<APIResponse> =
         return await authenticatedFetch(apiUrl, { method: 'POST' })
     } catch (error) {
         console.error('앨범 초대 실패:', (error as Error).message)
-        throw error
-    }
-}
-
-/**
- * 공동 작업자 삭제
- * @param albumId
- * @param userId
- * @returns
- */
-export const deleteCoworker = async (albumId: number, userId: number): Promise<APIResponse> => {
-    try {
-        const apiUrl = API_BASE_URL + `/api/album/${albumId}/members/${userId}`
-        return await authenticatedFetch(apiUrl, { method: 'DELETE' })
-    } catch (error) {
-        console.error('앨범 공동작업자 삭제 실패:', (error as Error).message)
         throw error
     }
 }
@@ -202,80 +120,6 @@ export const changeClusterTitle = async (albumId: string, clusterId: string, clu
         })
     } catch (error) {
         console.error('클러스터 이름 수정 실패:', (error as Error).message)
-        throw error
-    }
-}
-
-export const getAlbumComments = async (albumId: string) => {
-    try {
-        const apiUrl = API_BASE_URL + `/api/album/${albumId}/comments`
-        return await authenticatedFetch(apiUrl, {
-            method: 'GET',
-        })
-    } catch (error) {
-        console.error('댓글 조회 실패 : ', (error as Error).message)
-        throw error
-    }
-}
-
-export const addAlbumComments = async (albumId: string, comment: string) => {
-    try {
-        const apiUrl = API_BASE_URL + `/api/album/${albumId}/comments`
-        return await authenticatedFetch(apiUrl, {
-            method: 'POST',
-            body: JSON.stringify({ comments: comment }),
-        })
-    } catch (error) {
-        console.error('댓글 작성 실패 : ', (error as Error).message)
-        throw error
-    }
-}
-
-export const updateAlbumComments = async (albumId: string, commentsId: string, comment: string) => {
-    try {
-        const apiUrl = API_BASE_URL + `/api/album/${albumId}/comments/${commentsId}`
-        return await authenticatedFetch(apiUrl, {
-            method: 'PUT',
-            body: JSON.stringify({ comments: comment }),
-        })
-    } catch (error) {
-        console.error('댓글 수정 실패 : ', (error as Error).message)
-        throw error
-    }
-}
-export const deleteAlbumComments = async (albumId: string, commentsId: string) => {
-    try {
-        const apiUrl = API_BASE_URL + `/api/album/${albumId}/comments/${commentsId}`
-        return await authenticatedFetch(apiUrl, {
-            method: 'DELETE',
-        })
-    } catch (error) {
-        console.error('댓글 삭제 실패 : ', (error as Error).message)
-        throw error
-    }
-}
-//좋아요 조회
-export const getLikes = async (albumId: string) => {
-    try {
-        const apiUrl = API_BASE_URL + `/api/album/${albumId}/like`
-        return await authenticatedFetch(apiUrl, {
-            method: 'GET',
-        })
-    } catch (error) {
-        console.error('좋아요 조회 실패 : ', (error as Error).message)
-        throw error
-    }
-}
-
-export const controllLikes = async (albumId: string) => {
-    try {
-        const apiUrl = API_BASE_URL + `/api/album/${albumId}/like`
-        return await authenticatedFetch(apiUrl, {
-            method: 'POST',
-            body: JSON.stringify({}),
-        })
-    } catch (error) {
-        console.error('좋아요 제어 실패 : ', (error as Error).message)
         throw error
     }
 }
