41ee16fc24a7c0ffaef4587a80d7acf1c68b330e
zwen-yirochi <jijohn.vega@gmail.com>
2025-07-03 18:59:00 +0900

    feat:이미지 속성에 높이와 넓이 추가

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,10 +1,26 @@
 import { FC } from 'react'
 
+export interface ImageProps {
+    src: string
+    webp?: string // WebP 버전
+    width: number // 실제 이미지 크기
+    height: number // 실제 이미지 크기
+    alt: string
+}
+
 export interface GridItem {
     ElementType: FC<any> | React.ComponentType<any>
     element: number
     id?: string
-    props?: {}
+    props?: {
+        [key: string]: any
+        imageProps?: ImageProps
+    }
+}
+
+export interface OptimizedImageProps extends ImageProps {
+    className?: string
+    loading?: 'lazy' | 'eager'
 }
 
 export interface Picture {
