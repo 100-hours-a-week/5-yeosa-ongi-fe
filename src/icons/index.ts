e12a48445f2ecdd3f0e6882b797522b10b8035b2
zwen-yirochi <jijohn.vega@gmail.com>
2025-07-04 16:43:56 +0900

    fix:범용 아이콘 컴포넌트에 새로 추가된 커스텀 아이콘 추가

--- src/icons/index.ts
+++ src/icons/index.ts
@@ -1,4 +1,7 @@
 export { default as ArrowIcon } from './ArrowIcon'
-export { default as CheckIcon } from './checkIcon'
+export { default as CheckIcon } from './CheckIcon'
 export { default as CloseIcon } from './CloseIcon'
+export { default as EditIcon } from './EditIcon'
 export { default as HeartIcon } from './HeartIcon'
+export { default as ImageIcon } from './ImageIcon'
+export { default as LocationIcon } from './LocationIcon'
