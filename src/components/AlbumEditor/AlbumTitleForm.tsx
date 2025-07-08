f7570f4035795ca49627dab1d1e73f069d4f7374
zwen-yirochi <jijohn.vega@gmail.com>
2025-07-08 23:16:57 +0900

    refactor: 앨범 생성 관련 한글 문자열 상수 파일로 분리

--- src/components/AlbumEditor/AlbumTitleForm.tsx
+++ src/components/AlbumEditor/AlbumTitleForm.tsx
@@ -1,3 +1,4 @@
+import { ALBUM_TEXTS } from '@/constants/text'
 import { useState } from 'react'
 import TextField from '../common/TextField'
 
@@ -14,12 +15,12 @@ const AlbumTitleForm = ({ value, onChange, onValidationChange }: AlbumTitleFormP
     const validateTitle = (title: string) => {
         if (!title.trim()) {
             setIsValid(false)
-            setValidationMessage('제목을 입력해주세요.')
+            setValidationMessage(ALBUM_TEXTS.TITLE_REQUIRED)
             onValidationChange?.(false)
             return false
         } else if (title.length > 12) {
             setIsValid(false)
-            setValidationMessage('제목은 최대 12자까지 입력 가능합니다.')
+            setValidationMessage(ALBUM_TEXTS.TITLE_MAX_LENGTH)
             onValidationChange?.(false)
             return false
         }
@@ -31,8 +32,6 @@ const AlbumTitleForm = ({ value, onChange, onValidationChange }: AlbumTitleFormP
 
     const handleChange = (newTitle: string) => {
         onChange(newTitle)
-
-        // 입력 중에는 에러 메시지만 클리어
         if (!isValid) {
             setIsValid(true)
             setValidationMessage('')
@@ -42,7 +41,7 @@ const AlbumTitleForm = ({ value, onChange, onValidationChange }: AlbumTitleFormP
     return (
         <div className='flex flex-col w-full mt-4 mb-2'>
             <div className='flex items-center pb-2 mx-4 border-b border-gray-300'>
-                <div className='w-16 mx-4 font-medium text-gray-900'> 제목</div>
+                <div className='w-16 mx-4 font-medium text-gray-900'> {ALBUM_TEXTS.TITLE}</div>
                 <TextField
                     className='w-full text-md focus:outline-none'
                     value={value}
