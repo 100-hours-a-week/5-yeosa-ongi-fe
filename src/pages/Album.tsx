82f15a5291c03eb302bba43d3a64b2715bf3cc1c
zwen-yirochi <jijohn.vega@gmail.com>
2025-07-08 11:39:24 +0900

    album update

--- src/pages/Album.tsx
+++ src/pages/Album.tsx
@@ -24,13 +24,12 @@ import useModal from '../hooks/useModal'
 import useCollectionStore from '../stores/collectionStore'
 
 //Assets
-import iconDuplicated from '../assets/icons/icon_duplicated.svg'
-import iconShaky from '../assets/icons/icon_shaky.svg'
+import iconDuplicated from '../assets/icons/icon_duplicated.png'
+import iconShaky from '../assets/icons/icon_shaky.png'
+import images_icon from '../assets/icons/images_icon.png'
 
 //Types
 import Icon from '@/components/common/Icon'
-import ImageIcon from '@/icons/ImageIcon'
-import { Settings } from 'lucide-react'
 import { RawPicture } from '../types'
 
 interface ClusterInterface {
@@ -210,8 +209,8 @@ const Album = () => {
                         showCount={true}
                         onClick={() => setIsCommentsOpen(true)}
                     />
-                    <div className='absolute cursor-pointer right-6 text-md' onClick={handleSettingClick}>
-                        <Settings className='h-5 text-gray-500' />
+                    <div className='m-4 cursor-pointer text-md' onClick={handleSettingClick}>
+                        앨범 설정
                     </div>
                 </div>
             </div>
@@ -221,7 +220,7 @@ const Album = () => {
                     <div className='ml-4 font-sans text-md'>카테고리 </div>
                     <button onClick={() => navigate(`/album/${albumId}/전체`)} className='px-2'>
                         <div className='flex items-center '>
-                            <ImageIcon variant='multiple' className='w-4 h-4 text-gray-400' />
+                            <img src={images_icon} className='h-4'></img>
                             <div className='px-2 text-xs tracking-tighter'>
                                 전체 {allCollection?.count} 개 사진 보기
                             </div>
@@ -229,37 +228,17 @@ const Album = () => {
                     </button>
                 </div>
 
-                <div className='relative'>
-                    <div
-                        className='flex flex-row w-full gap-2 px-2 py-4 overflow-x-auto scrollbar-thin scrollbar-gray-light scrollbar-track-gray-light'
-                        onScroll={handleCategoryScroll}
-                    >
-                        {tagCollections &&
-                            tagCollections.map((category: Category, index: number) => (
-                                <Category title={category.name} pictures={category.pictures} albumId={albumId} />
-                            ))}
-                    </div>
-                    {showCategoryRightIndicator && (
-                        <div className='absolute top-0 right-0 flex items-center justify-end w-16 h-full pointer-events-none bg-gradient-to-l from-white to-transparent'>
-                            <div className='flex items-center justify-center w-8 h-8 mr-2 bg-white rounded-full shadow-sm bg-opacity-70'>
-                                <svg
-                                    xmlns='http://www.w3.org/2000/svg'
-                                    className='w-5 h-5 text-gray-500'
-                                    fill='none'
-                                    viewBox='0 0 24 24'
-                                    stroke='currentColor'
-                                >
-                                    <path
-                                        strokeLinecap='round'
-                                        strokeLinejoin='round'
-                                        strokeWidth={2}
-                                        d='M9 5l7 7-7 7'
-                                    />
-                                </svg>
-                            </div>
-                        </div>
-                    )}
-                </div>
+                <SideScrollableSection>
+                    {tagCollections &&
+                        tagCollections.map(category => (
+                            <Category
+                                key={category.name}
+                                title={category.name}
+                                pictures={category.pictures}
+                                albumId={albumId as string}
+                            />
+                        ))}
+                </SideScrollableSection>
             </div>
 
             <div className='m-4 mt-6'>
@@ -282,45 +261,39 @@ const Album = () => {
                 )}
             </div>
 
-            {duplicatedCollection?.count !== 0 && shakyCollection?.count !== 0 && (
-                <div className='m-4 mt-6'>
-                    <div className='ml-4 font-sans text-md'>검토해줘 </div>
+            <div className='m-4 mt-6'>
+                <div className='ml-4 font-sans text-md'>검토해줘 </div>
 
-                    {duplicatedCollection?.count !== 0 && (
-                        <div className='m-4'>
-                            <button
-                                className='flex items-center justify-between w-full bg-gray-100 border-0 border-b border-gray-200 focus:outline-none'
-                                onClick={() => navigate(`/album/${albumId}/중복`)}
-                            >
-                                <div className='flex items-center'>
-                                    <div className='flex items-center justify-center flex-shrink-0 w-8 h-8 mr-4 rounded-lg bg-gray-50'>
-                                        <img src={iconDuplicated}></img>
-                                    </div>
-                                    <div className='text-sm text-gray-dark'>중복된 사진</div>
-                                </div>
-                                <Icon name='arrow' className='text-gray-300' />
-                            </button>
+                <div className='m-4'>
+                    <button
+                        className='flex items-center justify-between w-full bg-gray-100 border-0 border-b border-gray-200 focus:outline-none'
+                        onClick={() => navigate(`/album/${albumId}/중복`)}
+                    >
+                        <div className='flex items-center'>
+                            <div className='flex items-center justify-center flex-shrink-0 w-8 h-8 mr-4 rounded-lg bg-gray-50'>
+                                <img src={iconDuplicated}></img>
+                            </div>
+                            <div className='text-sm text-gray-dark'>중복된 사진</div>
                         </div>
-                    )}
-
-                    {shakyCollection?.count !== 0 && (
-                        <div className='m-4'>
-                            <button
-                                className='flex items-center justify-between w-full bg-gray-100 border-0 border-b border-gray-200 focus:outline-none'
-                                onClick={() => navigate(`/album/${albumId}/흔들림`)}
-                            >
-                                <div className='flex items-center'>
-                                    <div className='flex items-center justify-center flex-shrink-0 w-8 h-8 mr-4 rounded-lg bg-gray-50'>
-                                        <img src={iconShaky}></img>
-                                    </div>
-                                    <div className='text-sm text-gray-dark'>흔들린 사진</div>
-                                </div>
-                                <Icon name='arrow' className='text-gray-300' />
-                            </button>
-                        </div>
-                    )}
+                        <Icon name='arrow' className='text-gray-300' />
+                    </button>
                 </div>
-            )}
+
+                <div className='m-4'>
+                    <button
+                        className='flex items-center justify-between w-full bg-gray-100 border-0 border-b border-gray-200 focus:outline-none'
+                        onClick={() => navigate(`/album/${albumId}/흔들림`)}
+                    >
+                        <div className='flex items-center'>
+                            <div className='flex items-center justify-center flex-shrink-0 w-8 h-8 mr-4 rounded-lg bg-gray-50'>
+                                <img src={iconShaky}></img>
+                            </div>
+                            <div className='text-sm text-gray-dark'>흔들린 사진</div>
+                        </div>
+                        <Icon name='arrow' className='text-gray-300' />
+                    </button>
+                </div>
+            </div>
 
             {/* 댓글 컨테이너 */}
             <CommentsContainer
