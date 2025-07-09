35955c486322bceb040c09f6fc37983b7c8fed48
zwen-yirochi <jijohn.vega@gmail.com>
2025-07-09 15:45:41 +0900

    refactor: 상수 분리

---
+++ src/constants/index.ts
@@ -0,0 +1,10 @@
+export const API_ENDPOINTS = {
+    USERS: '/api/users',
+    POSTS: '/api/posts',
+    AUTH: '/api/auth',
+}
+
+export const UI_HEIGHT = {
+    HEADER_HEIGHT: 56,
+    BANNER_HEIGHT: 72,
+}
